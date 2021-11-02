01e350b1d535f516d10412effb959b139f35e740
Rahul Kumar Patro <rahulkpatro@gmail.com>
2021-11-02 17:25:58 +0530

    added all routes

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# New
+<!-- # Felinos Technologies Assignment
+
+### Getting Started ⚡ :-
+
+
+ **1. Clone this repo by running the following command :-**
+ ```bash
+  git clone https://github.com/Rahul1582/Bloggers-Arena.git
+  cd Server
+ ```
+ 
+ **2. Now Install all the required packages by running the following commands :-**
+ ```bash
+  npm install 
+ ```
+ **3. Now start the React and Node server together by running the following command :-**
+ ```bash
+  node index.js
+ ```   
+   
+ **3.**  Open your browser and go to  `https://localhost:8000`**
+
+ **4.** **🎉  For using Postman `https://localhost:8000`** -->
--- routes/route.js
+++ routes/route.js
@@ -18,7 +18,7 @@ router.post("/adduser",(req,res)=>{
 
     if(name===undefined || email===undefined || mobile===undefined){
 
-        return res.json({status : 400, message : "User parameters missing! Adding User Unsuccessful",db});
+        return res.json({status : 400, message : "User parameters missing! Adding User Unsuccessful"});
     }
 
     else{
@@ -53,6 +53,206 @@ router.get("/getallusers",(req,res)=>{
 
 })
 
+
+// Add an order of an user
+router.post("/adduserorder",(req,res)=>{
+
+    const userid = req.body.userid;
+
+    const quantity = req.body.quantity;
+
+    const items = req.body.items;
+
+
+    var found= db.find(function(user, index) {
+        if(user.id == userid)
+        {
+            return true;
+        }        
+    });
+
+    if(found===undefined){
+        return res.json({status : 400, message : "User id not Found! Try with a different User id"});
+    }
+
+    else{
+
+        var ordernumber = 3000 + Math.floor(Math.random() * (1000) + 1);
+
+        var foundorder= found.orders.find(function(order, index) {
+            if(order.orderid == ordernumber)
+            {
+                return true;
+            }        
+        });
+
+        if(foundorder===undefined){
+
+            if(quantity===undefined || items===undefined ){
+
+                return res.json({status : 400, message : "User's Order parameters missing! Adding User's Order Unsuccessful"});
+            }
+
+            else{
+
+                var order = {orderid:ordernumber,quantity:quantity,items:items};
+
+                found.orders.push(order);
+        
+                return res.json({status : 200, message : "User Order added Successfully",db});
+
+            }
+
+        }
+
+        else{
+
+            return res.json({status : 400, message : "Order Already Exists"});
+
+        }
+        
+    }
+
+})
+
+
+// Get all orders of an user
+router.get("/getuserorders",(req,res)=>{
+
+    const userid = req.body.userid;
+
+
+    var found= db.find(function(user, index) {
+        if(user.id == userid)
+        {
+            return true;
+        }        
+    });
+
+    if(found===undefined){
+        return res.json({status : 400, message : "User id not Found! Try with a different User id"});
+    }
+
+    else{
+
+            var alluserorders=[];
+
+            alluserorders.push(found.orders);
+
+            return res.json({status : 200, message : "User Orders Found",alluserorders});
+
+        }
+    
+})
+
+
+// Update a particular order of an user
+router.put("/updateuserorder",(req,res)=>{
+
+    const userid = req.body.userid;
+
+    const orderid = req.body.orderid;
+
+    const quantity = req.body.quantity;
+
+    const items = req.body.items;
+
+
+    var found= db.find(function(user, index) {
+        if(user.id == userid)
+        {
+            return true;
+        }        
+    });
+
+    if(found===undefined){
+        return res.json({status : 400, message : "User id not Found! Try with a different User id"});
+    }
+
+    else{
+
+
+        var foundorder= found.orders.find(function(order, index) {
+            if(order.orderid == orderid)
+            {
+                return true;
+            }        
+        });
+
+        if(foundorder===undefined){
+            
+    
+            return res.json({status : 400, message : "User Order Doesn't exists"});
+        }
+
+        else{
+
+            if(quantity===undefined || items===undefined){
+                return res.json({status : 400, message : "Updating user's order not successful! Parametrs Missing"});
+            }
+
+            else{
+
+                foundorder.quantity=quantity;
+                foundorder.items=items;
+    
+                return res.json({status : 200, message : "User's Order Updated Successfully",db});
+
+            }
+
+        }
+        
+    }
+
+})
+
+
+// Delete a particular order of an user
+router.delete("/deleteuserorder",(req,res)=>{
+
+    const userid = req.body.userid;
+
+    const orderid = req.body.orderid;
+
+
+    var found= db.find(function(user, index) {
+        if(user.id == userid)
+        {
+            return true;
+        }        
+    });
+
+    if(found===undefined){
+        return res.json({status : 400, message : "User id not Found! Try with a different User id"});
+    }
+
+    else{
+
+
+        var foundorder= found.orders.findIndex(function(order, index) {
+            if(order.orderid == orderid)
+            {
+                return true;
+            }        
+        });
+
+        if(foundorder===undefined){
+            
+    
+            return res.json({status : 400, message : "User Order Doesn't exists"});
+        }
+
+        else{
+
+            found.orders.splice(foundorder, 1);
+
+            return res.json({status : 200, message : "User's Order Deleted Successfully",db});
+
+        }
+        
+    }
+
+})
 
 
 module.exports = router;
